79aea7e50b848a0cf57821e25cf0422fc10fdc59
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2017-12-31 22:50:42 +0700

    Release v0.001

---
+++ lib/WordLists/ID/Common.pm
@@ -0,0 +1,26 @@
+package WordLists::ID::Common;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Lists of common Indonesian words
+
+=head1 DESCRIPTION
+
+This distribution contains the following modules:
+
+=over
+
+=item * L<WordList::ID::Common::Wikipedia1000>
+
+=item * L<WordList::ID::Common::Wikipedia2500>
+
+=item * L<WordList::ID::Common::Wikipedia5000>
+
+=back
+
+
+=head1 SEE ALSO
+
+=cut
